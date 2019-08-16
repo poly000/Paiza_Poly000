f53de95f1f42c8be5e27e34eda4299fa0cb2b716
poly000 <34085039+poly000@users.noreply.github.com>
2019-08-17 02:29:23 +0800

    Create main.c

---
+++ 機械の総合病院/C/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+struct passwd {
+    char* text;
+    short len,status[256];
+    _Bool Num,Let;
+};
+int main(void) {    
+    struct passwd pass_;
+    unsigned int i;
+    char str[1000];
+    pass_.text = str;
+    fgets(str, sizeof(str), stdin);
+    
+    if ((pass_.len = strlen(pass_.text))>6) {
+        printf("Invalid");
+        return 0;
+    }
+    
+    for (i=0; i<0x100; i++) {
+        pass_.status[i] = 0;
+    }
+    
+    for (i=0; i<pass_.len; i++) {
+        pass_.status[(unsigned int)(pass_.text[i])]++;
+        if ((pass_.text[i] <= 'Z'  &&  pass_.text[i] >= 'A') || (pass_.text[i] <= 'z'  &&  pass_.text[i] >= 'a')) {
+            pass_.Let = 1;
+        } else {
+            if (pass_.text[i] <= '9'  &&  pass_.text[i] >= '0') {
+            pass_.Num = 1;
+            } else {
+                printf("Invalid");
+                return 0;
+            }
+        }
+    }
+    
+    if (!(pass_.Num&pass_.Let)) {
+        printf("Invalid");
+        return 0;
+    }
+    
+    for (i=0; i<0x100; i++) {
+        if (pass_.status[i] > 2) {
+            printf("Invalid");
+            return 0;
+        }
+    }
+    
+    printf("Valid");
+    return 0;
+}
+/ * standard: C99
+  * copyright: 2019 poly000
+  * LICENSE: Anti-996
+
+  * /
