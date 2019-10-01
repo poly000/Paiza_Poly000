a7d3c362369ac7a287711b59d24a2a9a10b9fae1
poly000 <34085039+poly000@users.noreply.github.com>
2019-10-01 10:06:08 +0800

    Create main.c

---
+++ C/砂漠の公園/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    unsigned int score;
+    unsigned int id;
+    unsigned int w,d,l;
+}group;
+
+int main(void) {
+    register unsigned int i,j;
+    unsigned int n;
+    group * p,* max;
+    
+    scanf("%u",&n);
+    getchar();
+    p = malloc(n*sizeof(group));
+    for (i=0; i<n; i++) {
+        (p+i)->id = i+1;
+        (p+i)->w = (p+i)->d = (p+i)->l = (p+i)->score = 0;
+        for (j=0; j<n; j++) {
+            switch (getchar()) {
+                case 'W':
+                    (p+i)->score += 2;
+                    (p+i)->w++;
+                    break;
+                case 'D':
+                    (p+i)->score++;
+                    (p+i)->d++;
+                    break;
+                case 'L':
+                    (p+i)->l++;
+                    break;
+            }
+        }
+        getchar();
+    }
+    max = p;
+    for (i=0; i<n; i++) {
+        if ((p+i)->score > max->score) {
+            max = p;
+        }
+    }
+    printf("%u %u %u %u %u",max->id,max->score,max->w,max->d,max->l);
+    return 0;
+}
