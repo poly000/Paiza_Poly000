80effbcc9f8076c2f64b1433be5f611278267e3b
poly000 <34085039+poly000@users.noreply.github.com>
2022-01-09 19:01:07 +0800

    clean up code

--- C/砂漠の公園/main.c
+++ C/砂漠の公園/main.c
@@ -8,39 +8,48 @@ typedef struct {
 }group;
 
 int main(void) {
-    register unsigned int i,j;
     unsigned int n;
-    group * p,* max;
-    
     scanf("%u",&n);
     getchar();
-    p = malloc(n*sizeof(group));
+    
+    group p[n];
+    
+    update_score(p);
+    group * max = max_score(p);
+    
+    printf("%u %u %u %u %u",max->id, max->score, max->w, max->d, max->l);
+    return 0;
+}
+
+group * max_score(const group p[const]) {
+    group * max = p;
+    for (i=1; i<n; i++) {
+        if (p[i].score > max->score) {
+            max = &p[i];
+        }
+    }
+    return max;
+}
+
+void update_score(const group p[]) {
     for (i=0; i<n; i++) {
-        (p+i)->id = i+1;
-        (p+i)->w = (p+i)->d = (p+i)->l = (p+i)->score = 0;
+        p[i].id = i+1;
+        p[i].w = p[i].d = p[i].l = p[i].score = 0;
         for (j=0; j<n; j++) {
             switch (getchar()) {
                 case 'W':
-                    (p+i)->score += 2;
-                    (p+i)->w++;
+                    p[i].score += 2;
+                    p[i].w++;
                     break;
                 case 'D':
-                    (p+i)->score++;
-                    (p+i)->d++;
+                    p[i].score++;
+                    p[i].d++;
                     break;
                 case 'L':
-                    (p+i)->l++;
+                    p[i].l++;
                     break;
             }
         }
         getchar();
     }
-    max = p;
-    for (i=0; i<n; i++) {
-        if ((p+i)->score > max->score) {
-            max = p+i;
-        }
-    }
-    printf("%u %u %u %u %u",max->id,max->score,max->w,max->d,max->l);
-    return 0;
 }
